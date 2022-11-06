e1d22577e3c4a5cd93029ea690c3ac1012f66bf0
Roshan kumar <84500586+Roshankumar-dotcom@users.noreply.github.com>
2022-11-06 23:13:06 +0530

    intial setup done

---
+++ index.js
@@ -0,0 +1,30 @@
+const { application } = require("express")
+const express = require("express")
+const userRouter = require("./routes/userRouter")
+const mongoose = require("mongoose")
+const adminRouter = require("./routes/adminRouter")
+const productRouter = require("./routes/productRouter")
+
+const cors = require('cors');
+
+
+const app = express()
+
+mongoose.connect("mongodb+srv://Roshan:roshan123@trail.33ilqpu.mongodb.net/?retryWrites=true&w=majority").then(()=> console.log("Database connected successfully")).catch(()=>{
+    console.log("Database connection error")
+})
+
+app.set('port', (process.env.PORT || 3300))
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.use(cors());
+app.use(userRouter)
+app.use(adminRouter)
+app.use(productRouter)
+
+
+
+app.listen(app.get('port'), ()=>{
+    console.log(`listening on port ${app.get('port')}`)
+})
